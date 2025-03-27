5c5703ad20e9830427e4322b84dc6f1f0982c613
tluwun <shylgacharsen345@gmail.com>
2025-03-27 19:49:28 +0200

    delete my password

--- nixos/modules/users.nix
+++ nixos/modules/users.nix
@@ -4,7 +4,6 @@
         isNormalUser = true;
         extraGroups = [ "audio" "video" "networkmanager" "wheel" "libvirt" ];
         shell = pkgs.fish;
-        password = "333672=Fedora";
     };
     
 
