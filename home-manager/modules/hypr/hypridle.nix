085766e0ff2bc6eb8a3b556a081a94b5383574e3
tluwun <shylgacharsen345@gmail.com>
2025-04-12 14:49:09 +0300

    change config of hypridle and add package brightnessctl

--- home-manager/modules/hypr/hypridle.nix
+++ home-manager/modules/hypr/hypridle.nix
@@ -3,18 +3,32 @@
         enable = true;
 
         settings = {
+            general = {
+                lock_cmd = "pidof || hyprlock";
+                before_sleep_cmd = "loginctl lock-session";
+                after_sleep_cmd = "hyprctl dispatch dpms on";
+            };
+
             listener = [
                 {
-                    timeout = 600;
+                    timeout = 150;
+                    on-timeout = "brightnessctl -s set 10";
+                    on-resume = "brightnessctl -r";
+                }
+                {
+                    timeout = 300;
                     on-timeout = "hyprlock";
+                }
+                {
+
+                    timeout = 350;
+                    on-timeout = "hyprctl dispatch dpms off";
+                    on-resume = "hyprctl dispatch dpms on && brightnessctl -r";
+                    
                 }
                 {
                     timeout = 1200;
                     on-timeout = "systemctl suspend";
-                }
-                {
-                    timeout = 3600;
-                    on-timeout = "systemctl poweroff";
                 }
             ];
         };
--- nixos/modules/packages.nix
+++ nixos/modules/packages.nix
@@ -48,5 +48,6 @@
             imv
             imagemagick
             ueberzugpp
+            brightnessctl
         ];
 }
