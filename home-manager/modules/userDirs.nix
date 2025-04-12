a4bc9933b0c25b55a9accc2c03e1af5666848c76
tluwun <shylgacharsen345@gmail.com>
2025-04-12 13:34:59 +0300

    fix xdg-user-dirs

--- home-manager/modules/userDirs.nix
+++ home-manager/modules/userDirs.nix
@@ -8,13 +8,13 @@
             
             createDirectories = true;
 
-            videos = "${config.home.homeDirectory}/Videos";
-            pictures = "${config.home.homeDirectory}/Pictures";
-            music = "${config.home.homeDirectory}/Music";
-            templates = "${config.home.homeDirectory}/Templates";
-            publicShare = "${config.home.homeDirectory}/Public";
-            documents = "${config.home.homeDirectory}/Documents";
-            desktop = "${config.home.homeDirectory}/Desktop";
+            videos = "${config.home.homeDirectory}Videos";
+            pictures = "${config.home.homeDirectory}Pictures";
+            music = "${config.home.homeDirectory}Music";
+            templates = "${config.home.homeDirectory}Templates";
+            publicShare = "${config.home.homeDirectory}Public";
+            documents = "${config.home.homeDirectory}Documents";
+            desktop = "${config.home.homeDirectory}Desktop";
         };
 
     };
