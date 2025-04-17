b78e2a2b756cddf3677ad625a533ab4df102bfd3
tluwun <shylgacharsen345@gmail.com>
2025-04-17 19:28:31 +0300

    add script for screenshot

--- nixos/modules/scripts.nix
+++ nixos/modules/scripts.nix
@@ -1,9 +1,12 @@
-{ pkgs, ...}: {
+{ pkgs, ... }: {
     environment.systemPackages = [ 
 
         (pkgs.writeShellScriptBin "waybar-weather" ''
             local=Kovel
             curl -s "wttr.in/$local?format=1"
+        '')
+        (pkgs.writeShellScriptBin "" ''
+            grim -g $(slurp) ~/Pictures/screenshots/screenshot-$(date | tr " " "_")  
         '')
     ];
 
