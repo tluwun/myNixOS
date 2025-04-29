c47550b20ebba391ba33475db1f7df459f97e9db
tluwun <shylgacharsen345@gmail.com>
2025-04-29 10:35:36 +0300

    add new network

--- nixos/modules/network.nix
+++ nixos/modules/network.nix
@@ -11,6 +11,14 @@
                 "UAinet-Home-11671" = {
                     pskRaw = "0894f52da7f732f081ccdb6889a481c1d401829dd9cdb7c9c3b5aa39bd399a37";
                 };
+
+                "Icraft 77" = {
+                    pskRaw = "de5270b4940ab31506ad9dc2203f75a2a9aedbc6591fcaf7986eb5ad944ecfb4";
+                };
+
+                # "" = {
+                #   pskRaw = "";
+                # };
             };
         };
     };
